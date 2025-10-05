771393c62e67a33225ea3d3b93aa1a7e170d8e70
Nikmorking <nikmor.2010.king@gmail.com>
2025-10-05 17:39:38 +0300

    rr

--- Kvanttim/character_body_3d.gd
+++ Kvanttim/character_body_3d.gd
@@ -6,29 +6,37 @@ const JUMP_VELOCITY = 4.5
 
 var mouse_sens = 0.3
 var camera_anglev=0
+var JumpVel: Vector3
+
+var mouse_sens = 0.1
+
+var isOnMenu = false
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
-		velocity += get_gravity() * delta
+		velocity.y += get_gravity().y * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor() and !isOnMenu:
 		velocity.y = JUMP_VELOCITY
-
+	
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir := Input.get_vector("A", "D", "W", "S")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	if direction:
+	if direction and !isOnMenu and is_on_floor():
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
 		$monk_character_Walking/AnimationPlayer.play("mixamo_com")
+		JumpVel = direction
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
-		$monk_character_Walking/AnimationPlayer.play("RESET")
-		
+		if is_on_floor():
+			velocity.x = move_toward(velocity.x * 1.8, 0, SPEED)
+			velocity.z = move_toward(velocity.z * 1.8, 0, SPEED)
+		if !is_on_floor():
+			velocity.x = move_toward(velocity.x * 2, JumpVel.x * 4.5, SPEED)
+			velocity.z = move_toward(velocity.z * 2, JumpVel.z * 4.5, SPEED)
 
 	move_and_slide()
 	#$Camera3D.rotate_y(deg_to_rad(-event.relative.x * mouse_sens))
@@ -40,7 +48,7 @@ func _physics_process(delta: float) -> void:
 
 
 func _input(event): 
-	if event is InputEventMouseMotion:
+	if event is InputEventMouseMotion and !isOnMenu:
 		if $Camera3D.rotation.x - deg_to_rad(event.relative.y) < deg_to_rad(90) and $Camera3D.rotation.x - deg_to_rad(event.relative.y) > deg_to_rad(-70):
 			$Camera3D.rotation += Vector3(deg_to_rad(-event.relative.y * mouse_sens), 0, 0)
 		rotation += Vector3(0 ,deg_to_rad(-event.relative.x * mouse_sens), 0)
--- Kvanttim/node_3d.gd
+++ Kvanttim/node_3d.gd
@@ -9,4 +9,14 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("Escape"):
+		if !$CharacterBody3D.isOnMenu:
+			$CharacterBody3D.isOnMenu = true
+			#$CharacterBody3D.velocity = Vector3(0, 0, 0)
+			$CanvasLayer.show()
+			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+		else:
+			$CharacterBody3D.isOnMenu = false
+			$CanvasLayer.hide()
+			Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	pass
